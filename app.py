494cd104ed9a3f9a083f61176fb570579d5a42a5
shun0157 <playful0157@gmail.com>
2020-11-02 23:48:51 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import qrcode
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def get():
+    text = request.args.get("t")
+    qr = qrcode.QRCode()
+    qr.add_data(text)
+    qr.make()
+    matrix = qr.get_matrix() if text else []
+    return render_template('index.html', matrix=matrix, text=text)
+
+
+if __name__ == '__main__':
+    app.run()
